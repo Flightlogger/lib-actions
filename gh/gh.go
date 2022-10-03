e0315a47098607d4f560da97ce6ba8bd4b53de91
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2022-10-03 11:41:18 +0200

    Get current pull-request method.

--- gh/gh.go
+++ gh/gh.go
@@ -1,7 +1,77 @@
 package gh
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
-func FooFunc() {
-  fmt.Println("Does this work?")
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+type Client struct {
+  ctx context.Context
+  GithubRef string
+  Org string
+  Repo string
+
+  ghClient *github.Client
 }
+
+func CreateClient(ctx context.Context, org string, repo string, apiKey string) (*Client) {
+  token := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apiKey})
+  tokenClient := oauth2.NewClient(ctx, token)
+  ghClient := github.NewClient(tokenClient)
+
+  return &Client {
+    Org: org,
+    Repo: repo,
+    GithubRef: os.Getenv("GITHUB_REF"),
+
+    ghClient: ghClient,
+    ctx: ctx,
+  }
+}
+
+// Gets the pull-request(s) associated with the current action.
+func (client Client) GetCurrentPullRequests() ([]*github.PullRequest, error) {
+  eventName := os.Getenv("GITHUB_EVENT_NAME")
+  if eventName == "pull_request" {
+    // Ref is a PR. Fetch PR by PR number, supplied by ref.
+    prNum, err := strconv.Atoi(strings.Split(client.GithubRef, "/")[2])
+    if err != nil { return nil, errors.New("Failed to split and/or parse GitHub pull-request ref string.") }
+    pr, _, err := client.ghClient.PullRequests.Get(client.ctx, client.Org, client.Repo, prNum)
+    if err != nil { return nil, errors.New("Failed to get pull-request from GitHub API.") }
+    
+    return []*github.PullRequest{pr}, nil
+  } else if eventName == "push" {
+    // Ref is a branch. Get PR by searching for branch, supplied by ref.
+    opts := github.PullRequestListOptions {
+      Head: fmt.Sprintf("%s: %s", client.Repo, client.GithubRef),
+    }
+    prs, _, err := client.ghClient.PullRequests.List(client.ctx, client.Org, client.Repo, &opts)
+    if err != nil { return nil, errors.New("Failed to list pull-requests from GitHub API.") }
+
+    return prs, nil
+  } else {
+    return nil, errors.New("Unsupported action event type.")
+  }
+}
+
+// func startPipeline() {
+//   fmt.Printf("Attempting to start pipeline for ref: %s\n", headRef)
+//   num, err := cciClient.CreatePipeline(headRef)
+//   if err != nil {
+//     if safeMatchString("Ref not found", err) {
+//       fmt.Printf("CCI: Ref %s not found. Ignoring.\n", headRef)
+//       return
+//     }
+//     panic(err)
+//   }
+// 
+//   fmt.Printf("CCI: Started pipline #%d", num)
+// }
+
--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module github.com/Flightlogger/lib-actions
 
 go 1.19
 
-require github.com/imroc/req/v3 v3.24.0
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/imroc/req/v3 v3.24.0
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
+)
 
 require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lucas-clemente/quic-go v0.28.1 // indirect
@@ -24,5 +30,7 @@ require (
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.12 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
